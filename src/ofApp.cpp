6c9a1d7727eab0de0cf1b7861b274539da7ff3fb
Lisalotte <lisa.pothoven@xs4all.nl>
2018-05-18 11:17:54 +0200

    Final version

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,102 +1,97 @@
 #include "ofApp.h"
 
-//--------------------------------------------------------------
-void ofApp::setup(){
-	//just set up the openFrameworks stuff
+/*
+This program shows you a dot (1D) that transforms into a line, which transforms into a circle (2D).
+(In the code, the dot and the line are oddly shaped ellipses.) The circle, in turn, transforms
+into a sphere (3D) once you enter it. Then, when you are inside the sphere, a black hole is created.
+The black hole is a tunnel that will compress you back to 1D, into a dot.
+*/
+
+void ofApp::setup() {
 	ofSetFrameRate(60);
 	ofSetVerticalSync(true);
-	ofBackground(0);  // Clear the screen with a black color	
+	ofBackground(0);
 
-	cylinder.set(250, 1);
-	cyline.set(0.5, 500);
+	cylinder.set(1, 1);		// Create a dot
 
-	firstTime = true;
 	lineToCircle = false;
 	circleToSphere = false;
 	blackHole = false;
-
-	lookat = 0;
-
-	blackHolePos = ofVec3f(0, 0, -10000);
-
-	x_self = y_self = z_self = 0;
+	drawSphere = false;
+	allowHorizontalOrbit = true;
+	allowVerticalOrbit = true;
 
 	lineLength = 1;
 	distance = 500.f;
-	roll = angleH = angleV = 0;
+	angleH = angleV = 0;
 	alpha = 0;
-	firstAngle = 0;
-	sphereanglecounter = 0;
+	counter = 0;
+	counter2 = 0;
+	lookat = 0;
 
-	// setup the camera at initial viewing angles
+	blackHolePos = ofVec3f(0, 0, -50000);
+
+	// Setup the camera's initial viewing angles
 	cam.orbit(0, 0, distance);
 	cam.orbit(0, 0, distance);
 
-	allowCamRotate = false;
-	drawSphere = false;
-	
+	// Setup the coordinates for 300 stars
 	for (int i = 0; i < 300; i++) {
-		float x = ofRandomf() * 2000 - 1000;
-		float y = ofRandomf() * 2000 - 1000;
-		float z = ofRandomf() * 2000 - 1000;
-		positions[i][0] = x;
-		positions[i][1] = y;
-		positions[i][2] = z;
-
 		sphericalPositions[i][0] = ofRandomuf() * 360;
 		sphericalPositions[i][1] = ofRandomuf() * 360;
 		sphericalPositions[i][2] = ofRandomuf() * 1000 + 1000;
 	}
-	counter = counter2 = 0;
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-	allowCamRotate = false;
+void ofApp::update() {
 	if (lineLength >= lineMax) {
 		lineToCircle = true;
 	}
 
-	if (angleV >= 360) {
+	// Reset angles when they reach 360
+	if (angleV == 360) {
 		angleV = 0;
 	}
-	if (angleH >= 360) {
+	if (angleH == 360) {
 		angleH = 0;
 	}
-	if (angleV <= -360) {
+	if (angleV == -360) {
 		angleV = 0;
 	}
-	if (angleH <= -360) {
+	if (angleH == -360) {
 		angleH = 0;
 	}
-	
-	// line (1D)
-	if (!lineToCircle) { 
-		if (ofGetKeyPressed(OF_KEY_UP) || ofGetKeyPressed(OF_KEY_DOWN)) { // only when up or down is pressed
-			if (angleV <= 180) cam.orbit(0, angleV, distance);
-			//allowCamRotate = true;
-			//angleH = 0;
-			if (lineLength < lineMax && (angleV > 10 || angleV < -10)) lineLength += 1;
-		}
-	}
-	// circle (2D)
-	else if (!circleToSphere) { // keep rotating the sphere
-		counter++;
-		//if (angleH <= 90 && angleH >= -90) {
-		//	allowCamRotate = true;
-			//cam.orbit(angleH, angleV, distance);
 
-		if (counter <= 180 ){
-			//angleV = 0;
-			//allowCamRotate = true;
+	// Line (1D)
+	if (!lineToCircle) { // Orbit vertically around the line
+		allowHorizontalOrbit = false;
+		cam.orbit(angleH, angleV, distance);
+		lineLength = abs(angleV) * (299 / 90) + 1; // Make the line look longer when you orbit vertically around it
+	}
+
+	// Circle (2D)
+	else if (lineToCircle) { // Orbit around the circle
+		counter++;
+		if (counter <= 900) {
+			allowHorizontalOrbit = true;
 			cam.orbit(angleH, angleV, distance);
 		}
-		else if (angleH < 90 && angleH > -90) { // als counter groter is dan 300 en de cirkel nog niet plat is
-			//angleV = 0;
-			//allowCamRotate = true;
+		else if (angleV < 180 && angleV > -180) {
+			allowHorizontalOrbit = true;
 			cam.orbit(angleH, angleV, distance);
 		}
-		else { // switch to 3D
+		else if (counter <= 1200) {
+			allowHorizontalOrbit = true;
+			allowVerticalOrbit = false;
+			cam.orbit(angleH, angleV, distance);
+		}
+		else if (angleH < 90 && angleH > -90) {
+			allowHorizontalOrbit = true;
+			allowVerticalOrbit = false;
+			cam.orbit(angleH, angleV, distance);
+		}
+		else { // Switch to 3D
 			lineToCircle = false;
 			circleToSphere = true;
 		}
@@ -108,25 +103,17 @@ void ofApp::update(){
 		camPosition = cam.getGlobalPosition();
 		float distance = sqrtf(camPosition[0] * camPosition[0] + camPosition[1] * camPosition[1] + camPosition[2] * camPosition[2]);
 
-		//if (distance <= lineLength*0.4) {
-		//	cerr << "z" << distance << endl;
-		//	if (alpha < 255) alpha++;
-		//	drawSphere = true;
-		//}
 		if (!drawSphere) {
-			if (ofGetKeyPressed(OF_KEY_UP)) { // move forwards into the sphere 
-				//if (z_self > -10) z_self -= 0.5; // set maximum backwards speed to -10
+			if (ofGetKeyPressed(OF_KEY_UP)) { // Move forwards into the sphere 
 				cam.dolly(-2);
 			}
-			else if (ofGetKeyPressed(OF_KEY_DOWN)) { // register keypress down: move backwards into the sphere
-			 //if (z_self < 10) z_self += 0.5; // set maximum speed to 10
+			else if (ofGetKeyPressed(OF_KEY_DOWN)) { // Register keypress down: move backwards into the sphere
 				cam.dolly(2);
 			}
 		}
-
 		if (!blackHole && distance <= 50) {
 			while (distance > 5) {
-				cam.setPosition(camPosition-camPosition.operator*(0.1));
+				cam.setPosition(camPosition - camPosition.operator*(0.1));
 				camPosition = cam.getGlobalPosition();
 				distance = sqrtf(camPosition[0] * camPosition[0] + camPosition[1] * camPosition[1] + camPosition[2] * camPosition[2]);
 			}
@@ -136,9 +123,12 @@ void ofApp::update(){
 				alpha++;
 			}
 			drawSphere = true;
+
 		}
 
 		if (drawSphere) {
+			allowHorizontalOrbit = false;
+			allowVerticalOrbit = false;
 			if (ofGetKeyPressed(OF_KEY_UP)) {
 				cam.tilt(1);
 			}
@@ -153,23 +143,20 @@ void ofApp::update(){
 			}
 			counter2++;
 
-			if (counter2 > 120) {
+			if (counter2 > 420) {
 				blackHole = true;
 				if (alpha > 0) alpha--;
 				else {
 					drawSphere = false;
-					cerr << "yup";
 				}
 			}
 		}
 		if (blackHole) {
+			allowHorizontalOrbit = false;
+			allowVerticalOrbit = false;
 			camPosition = cam.getGlobalPosition();
-			/*float xx = pow(blackHolePos[0] - camPosition[0], 2);
-			float yy = pow(blackHolePos[1] - camPosition[1], 2);
-			float zz = pow(blackHolePos[2] - camPosition[2], 2);
-			float bhDistance = sqrt(xx + yy + zz);*/
 			float bhDistance = camPosition.distance(blackHolePos);
-			
+
 			ofPoint dir = cam.getLookAtDir();
 			dir = dir.normalize();
 			float xdir = -1 * dir[0];
@@ -181,23 +168,49 @@ void ofApp::update(){
 					camPosition = cam.getGlobalPosition();
 					cam.lookAt(dir + (ofPoint(xdir, ydir, zdir - camPosition[2] - 1).operator/(500.0)));
 				}
-				cam.lookAt(ofPoint(0, 0, -10000));
+				cam.lookAt(ofPoint(0, 0, -50000));
 				firstTime = false;
 				time = 0;
 			}
-			
-			/* 
-			
-			float bhDistance = camPosition.distance(blackHolePos);
-			ofVec3f orientation = cam.getLookAtDir();		
-			cam.lookAt(orientation-orientation.operator*(0.1));
-			
-			*/
+
 			if (camPosition[2] > blackHolePos[2]) {
 				camPosition = cam.getGlobalPosition();
 				//bhDistance = camPosition.distance(blackHolePos);
-				cam.setPosition(camPosition + ofPoint(0,0,-5 - 0.01*time*time));
+				cam.setPosition(camPosition + ofPoint(0, 0, -5 - 0.01*time*time));
 				time = time + 1;
+			}
+			else {
+				ofBackground(0);
+
+				cylinder.set(1, 1);		// Create a dot
+
+				lineToCircle = false;
+				circleToSphere = false;
+				blackHole = false;
+				drawSphere = false;
+				allowHorizontalOrbit = true;
+				allowVerticalOrbit = true;
+
+				lineLength = 1;
+				distance = 500.f;
+				angleH = angleV = 0;
+				alpha = 0;
+				counter = 0;
+				counter2 = 0;
+				lookat = 0;
+
+				blackHolePos = ofVec3f(0, 0, -50000);
+
+				// Setup the camera's initial viewing angles
+				cam.orbit(0, 0, distance);
+				cam.orbit(0, 0, distance);
+
+				// Setup the coordinates for 300 stars
+				for (int i = 0; i < 300; i++) {
+					sphericalPositions[i][0] = ofRandomuf() * 360;
+					sphericalPositions[i][1] = ofRandomuf() * 360;
+					sphericalPositions[i][2] = ofRandomuf() * 1000 + 1000;
+				}
 			}
 			if (ofGetKeyPressed(OF_KEY_UP)) {
 				cam.tilt(1);
@@ -212,104 +225,26 @@ void ofApp::update(){
 				cam.pan(-1);
 			}
 		}
-		/*
-		if (drawSphere) { // && distance >= lineLength*0.4) {
-			float theta = ofRadToDeg(sinh(camPosition[1] / distance)); // lineLength*0.5));
-			float phi = ofRadToDeg(tanh(camPosition[2] / camPosition[0]));
-		
-			if (distance >= lineLength*0.4) {
-				if (ofGetKeyPressed(OF_KEY_UP)) { // move forwards into the sphere 
-					//theta = theta + 2;
-					//phi += 5;
-					cam.dolly(1);
-					cerr << "fw" << endl;
-				}
-				if (ofGetKeyPressed(OF_KEY_DOWN)) { // move forwards into the sphere 
-					//theta = theta - 2;
-					//phi -= 5;
-					cam.dolly(-1);
-					cerr << "bw" << endl;
-				}
-			}
-			else {
-				cam.dolly(2);
-			}
-
-			// 0 - 90
-			if (camPosition[0] >= 0 && camPosition[1] < 0) {
-				phi = phi + 0;
-			}
-			//  90 - 180
-			if (camPosition[0] < 0 && camPosition[1] < 0) {
-				phi = phi + 90;
-			}
-			// 180 - 270
-			if (camPosition[0] < 0 && camPosition[1] >= 0) {
-				phi = phi + 180;
-			}
-			//270 - 360
-			if (camPosition[0] >= 0 && camPosition[1] >= 0) {
-				phi = phi + 270;
-			}			
-
-			cerr << theta << ", " << phi << endl;
-
-			//float theta = (pos.angle(ofVec3f(0, 0, 0)));
-			
-			//look = camPosition.getNormalized();
-			//look2 = cam.getUpDir().getNormalized();
-			
-			//look = look.perpendicular(look2);
-			
-			theta = ofDegToRad(theta + 90);
-			phi = ofDegToRad(phi);
-			
-			look[0] = distance * sin(theta + 0) * cos(phi);
-			look[1] = distance * sin(theta + 0) * sin(phi);
-			look[2] = distance * cos(theta + 0);
-			//look = look + camPosition;
-
-			if (distance >= lineLength*0.5) {
-				//cam.lookAt(look);
-				//cam.move(look);
-			}
-		}
-		*/
-	
-		
 	}
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
 
-	//ofEnableLighting();
-	//ofEnableSmoothing();
 	ofEnableDepthTest();
 
-	//glMatrixMode(GL_PROJECTION);
-	//cam.setFov(180);
-	//cam.setNearClip(1);
-	//cam.setFarClip(3000);
-	cam.begin(); // enable camera, everything onwards will appear on the screen
+	cam.begin(); // Enable camera, everything onwards will appear on the screen
 
-	drawStars(); // draw background stars
-	if (allowCamRotate) {
-		cam.orbit(angleH, angleV, distance);
-	}
+	drawStars();
 
-	//if (circleToSphere) {
-	//	cam.dolly(z_self); // move along local z-axis in back of forward direction
-	//}
+	ofPushMatrix(); // Global positioning
 
-	ofPushMatrix(); // global positioning
+	ofPushMatrix(); // Object positioning
 
-	ofPushMatrix(); // object positioning
-	
-	ofNoFill(); // disable fill color
-	ofSetColor(255); // set color to white
+	ofNoFill(); // Disable fill color
+	ofSetColor(255); // Set color to white
 
-	// BLACK HOLE IN YOUR FACE
+	// Black Hole in your face
 	if (blackHole) {
 		// at random orientation
 		float t, p;
@@ -348,7 +283,7 @@ void ofApp::draw(){
 				//ofVec3f third(1.5 * sin(t) * cos(p), -2 * sin(t) * sin(p), cos(t));
 				//third.operator*(rad);
 				t = 180 * 0.0174533;
-				ofVec3f fourth(goTo[0] - 100 * rad * sin(t) * cos(p), goTo[1], goTo[2] - 15000 * rad);
+				ofVec3f fourth(goTo[0] - 0 * rad * sin(t) * cos(p), goTo[1], goTo[2] - 50000 * rad);
 				//ofVec3f fourth(1.5 * sin(t) * cos(p), -5 * sin(t) * sin(p), cos(t));
 
 				//fourth.operator*(rad);
@@ -361,105 +296,51 @@ void ofApp::draw(){
 	}
 
 	ofPushMatrix();
-	if (!lineToCircle) {
-		cylinder.setRadius(lineLength/2);
+	if (!lineToCircle) { // If the line still looks like a line
+						 // Set settings for the circle
+		cylinder.setRadius(lineLength / 2);
+		cylinder.setResolution(50, 50, 50);
+		cylinder.setCapped(false);
 	}
-	ofRotateZ(90);
-	cylinder.setResolution(50, 50, 50);
-	cylinder.setCapped(false);
-	cylinder.draw();
+	// Draw a circle and make sure that it is rotated 90 degrees over the Z axis, so at first sight it appears to be a line
+	ofRotateZ(90); //
+	if (!drawSphere) cylinder.draw();
 
 	if (drawSphere) {
 		ofSetColor(255, 255, 255, alpha);
 		ofDrawSphere(lineLength*0.5);
 	}
 	ofSetColor(255);
-	
+
 	ofPopMatrix();
 
-	ofPopMatrix(); // objects
+	ofPopMatrix(); // Objects
 
-	ofPopMatrix(); // global
+	ofPopMatrix(); // Global
 
 	cam.end();
-
-	//ofDisableLighting();
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key) {
 	switch (key) {
 	case OF_KEY_LEFT:
-		if (lineToCircle) angleH += 1; // rotate around line -> circle
+		if (allowHorizontalOrbit) angleH += 1;
 		break;
 	case OF_KEY_RIGHT:
-		if (lineToCircle) angleH -= 1; // rotate around line -> circle
+		if (allowHorizontalOrbit) angleH -= 1;
 		break;
 	case OF_KEY_UP:
-		angleV -= 1;
+		if (allowVerticalOrbit) angleV -= 1;
 		break;
 	case OF_KEY_DOWN:
-		angleV += 1;
-		break;
-	case 'b':
-		blackHole = true;
+		if (allowVerticalOrbit) angleV += 1;
 		break;
 	}
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
-
-//--------------------------------------------------------------
-void ofApp::drawStars() { 
-
+void ofApp::drawStars() {
 	for (int i = 0; i < 300; i++) {
 		ofPushMatrix();
 		// azimuth
@@ -469,27 +350,19 @@ void ofApp::drawStars() {
 		ofRotateY(sphericalPositions[i][1]);
 		// radius
 		ofTranslate(sphericalPositions[i][2], 0, 0);
-		
-		//float x = positions[i][0];
-		//float y = positions[i][1];
-		//float z = positions[i][2];
-//		ofDrawSphere(ofPoint(x, y, z), 2);
+
 		ofDrawSphere(ofPoint(0, 0, 0), 2);
 		ofPopMatrix();
 	}
-
 }
 
-// Calculate point at time t, on Bezier Curve corresponding to control points (p0, p1, p2, p3)
-ofVec3f ofApp::CalculateBezier(GLfloat t, ofVec3f p0, ofVec3f p1, ofVec3f p2, ofVec3f p3)
-{
+ofVec3f ofApp::CalculateBezier(GLfloat t, ofVec3f p0, ofVec3f p1, ofVec3f p2, ofVec3f p3) {
 	GLfloat u = 1 - t;
 	GLfloat uu = u * u;
 	GLfloat uuu = uu * u;
 	GLfloat tt = t * t;
 	GLfloat ttt = tt * t;
-	
+
 	ofVec3f bezier = (p0.operator*(uuu)) + p1.operator*(3 * uu * t) + p2.operator*(3 * u * tt) + p3.operator*(ttt);
 	return bezier;
 }
-
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,56 +2,38 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
+public:
 
-		ofCylinderPrimitive cylinder, cyline;
-		ofNode myNode;
-		ofCamera cam;
-		ofVec3f camPosition;
-		ofPolyline line;
-		ofMesh mesh;
-		ofVec3f look, look2, blackHolePos;
-		
-		float x_rot, y_rot, z_rot;
-		float x_self, y_self, z_self;
-		float lineLength, angleH, angleV, distance, roll;
-		float alpha;
-		float firstAngle;
-		float lookat;
-		const float lineMax = 300;
-		const int delayMax = 1000;
+	ofCylinderPrimitive cylinder;	//Cylinder that we transform to show a dot, line and circle on screen
+	ofNode myNode;
+	ofCamera cam;
+	ofVec3f camPosition;
+	ofPolyline line;
+	ofMesh mesh;
+	ofVec3f look, look2, blackHolePos;
 
-		float positions[300][3];
-		float sphericalPositions[300][3];
+	float lineLength, angleH, angleV, distance, alpha;
+	const float lineMax = 300;	// Final linelength and the diameter of the circle
+	float sphericalPositions[300][3];	// Array that stores the star coordinates
+	float time;
+	float lookat;
 
-		bool lineToCircle;
-		bool circleToSphere;
-		bool allowCamRotate;
-		bool drawSphere;
-		bool blackHole;
-		int counter, counter2;
-		int sphereanglecounter;
+	bool lineToCircle; // True when lineLength equals the diameter of the circle
+	bool circleToSphere;
+	bool allowCamRotate;
+	bool drawSphere;
+	bool blackHole;
+	bool allowHorizontalOrbit;
+	bool allowVerticalOrbit;
+	bool firstTime;
+	int counter, counter2; // A counter that makes sure that the user has some time to play with the circle, orbitting around it. When the counter has reached a maximum, the user will stop orbitting around it vertically/horizontally when it displayed flat on the screen
 
-		bool firstTime;
-		float time;
-
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		void drawStars();
-		ofVec3f CalculateBezier(GLfloat t, ofVec3f p0, ofVec3f p1, ofVec3f p2, ofVec3f p3);
+	void setup();
+	void update();
+	void draw();
+	void keyPressed(int key);
+	void drawStars();	// Draw background stars
+	ofVec3f CalculateBezier(GLfloat t, ofVec3f p0, ofVec3f p1, ofVec3f p2, ofVec3f p3);	// Calculate point at time t, on Bezier Curve corresponding to control points (p0, p1, p2, p3)
 };
